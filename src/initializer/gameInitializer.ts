9fe31570669fd1728bab275f2d7e709c8a3a09cf
Eiichi Kihara <107760158+shihu-14@users.noreply.github.com>
2026-08-16 22:30:43 +0900

    fix gameplay state initialization

--- src/initializer/gameInitializer.ts
+++ src/initializer/gameInitializer.ts
@@ -3,15 +3,19 @@ import { GhostData } from '../data/ghostData';
 import { stageReset } from '../controller/stageController';
 import { settings } from '../settings';
 
-export const gameInitializer = (playerData :PlayerData, ghostDatas:GhostData[]) => {
+export const resetActorPositions = (playerData: PlayerData, ghostDatas: GhostData[]) => {
+  const playerNow = performance.now() / 1000;
+  const ghostNow = Date.now() / 1000;
+
   playerData.x = 10;
   playerData.y = 10;
   playerData.targetX = 10;
   playerData.targetY = 10;
   playerData.preX = 10;
   playerData.preY = 10;
+  playerData.start = playerNow;
+  playerData.direction = 'None';
   playerData.have = 0;
-  playerData.nouhin = 0;
 
   for (let i = 0; i < ghostDatas.length; i += 1) {
     ghostDatas[i].gx = 5;
@@ -20,7 +24,15 @@ export const gameInitializer = (playerData :PlayerData, ghostDatas:GhostData[]) 
     ghostDatas[i].gtargetY = 5;
     ghostDatas[i].gpreX = 5;
     ghostDatas[i].gpreY = 5;
+    ghostDatas[i].gstart = ghostNow;
+    ghostDatas[i].gdirect = 'gNone';
   }
+};
+
+export const gameInitializer = (playerData: PlayerData, ghostDatas: GhostData[]) => {
+  resetActorPositions(playerData, ghostDatas);
+  playerData.nouhin = 0;
+
   stageReset();
   settings.start = performance.now();
 };
